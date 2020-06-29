d88c436255484cc9736e5b2e02d8ea2f99226d5c
Sydney Jenkins <sydjenkins168@gmail.com>
2020-06-29 03:34:11 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,9 +10,10 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with("A")
-      winner=name
+      winner = name
     end
   end 
+  winner 
 end
 
 
